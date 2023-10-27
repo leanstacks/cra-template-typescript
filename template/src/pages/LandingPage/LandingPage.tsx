c29cfc7d1225fce36a6d6598925751aabc96e63e
Matthew Warman <warmanmr@gmail.com>
2023-10-27 14:10:22 -0400

    LSUITE-45 interfaces and content

--- template/src/pages/LandingPage/LandingPage.tsx
+++ template/src/pages/LandingPage/LandingPage.tsx
@@ -38,11 +38,23 @@ const LandingPage = () => {
       </div>
 
       <div className="mb-4">
-        <Text variant={TextVariant.Heading2}>Users</Text>
+        <Text variant={TextVariant.Heading2} className="mb-4">
+          Users
+        </Text>
+        <div className="mb-2 inline-block rounded bg-white/10 p-2">
+          &#9432; This is a sample component which fetches and renders a list of users. It
+          illustrates the use of React Query hooks to fetch and cache data.
+        </div>
         {usersSuccess && users.map((user) => <div key={user.id}>{user.name}</div>)}
       </div>
 
-      <div className="text-sm uppercase">
+      <div className="mb-4">
+        <Text variant={TextVariant.Heading2} className="mb-4">
+          Configuration
+        </Text>
+        <div className="mb-2 inline-block rounded bg-white/10 p-2">
+          &#9432; This is a sample which illustrates using the React Context for Configuration.
+        </div>
         <div>App Version: {config.REACT_APP_VERSION_ID}</div>
       </div>
     </div>
